d8cc9c8a53efd9b6a6836f19844be285e8f8c9f9
Neslihan-Edinc <neslihan@archonph.com>
2024-08-15 11:11:15 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,25 @@
+def merge_sorted_lists(list1, list2):
+    merged_list = []
+    i, j = 0, 0
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged_list.append(list1[i])
+            i += 1
+        else:
+            merged_list.append(list2[j])
+            j += 1
+    
+    while i < len(list1):
+        merged_list.append(list1[i])
+        i += 1
+    
+    while j < len(list2):
+        merged_list.append(list2[j])
+        j += 1
+    
+    return merged_list
+user_input1=input("enter a list1: ")
+user_input2=input("enter a list2: ")
+merge_sorted_lists([user_input1+user_input1])
+
